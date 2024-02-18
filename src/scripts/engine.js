0f565d778df28aca9a95144fcc6bdb8b9c8ff714
raizarochaw <raizarocha8@gmail.com>
2024-02-17 21:25:23 -0300

    feat: adiciona funcionalidade para o botão teclas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,12 +1,12 @@
 // pega todas as teclas(key) do documento principal
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
-
 // pega o input de volume
 const volumeSlider = document.querySelector(".volume-slider input");
+// pega o input da keys check (teclas)
+const keysCheck = document.querySelector(".keys-check input");
 
 // as teclas só serão tocadas se estiverem dentro desse array
 let mapedKeys = [];
-
 // variável criada para acessar o caminho dos audios
 let audio = new Audio("src/tunes/a.wav");
 
@@ -14,13 +14,10 @@ let audio = new Audio("src/tunes/a.wav");
 const playTune = (key) => {
   audio.src = `src/tunes/${key}.wav`;
   audio.play();
-
   // pega no elemento html a data-key equivalente a letra(tecla) tocada 
   const clickedKey = document.querySelector(`[data-key="${key}"]`);
-
   // adiciona dinamicamente a classe "active"
   clickedKey.classList.add("active");
-
   // cria um contador que a cada 150ms removerá a classe "active"
   setTimeout(() => {
     clickedKey.classList.remove("active");
@@ -47,5 +44,13 @@ const handleVolume = (e) => {
   audio.volume = e.target.value;
 };
 
-// sempre que o input de volume for manipulado, chama a função handleVolume
+// o toggle verifica se existe a classe hide na lista, se tiver ele remove, se não tiver ele adiciona
+const showHideKeys = () => {
+  pianoKeys.forEach(key => key.classList.toggle("hide"));
+}
+
+// quando o input de volume é manipulado, chama a função handleVolume
 volumeSlider.addEventListener("input", handleVolume);
+
+// quando o botão de teclas é clicado, chama a função showHideKeys
+keysCheck.addEventListener("click", showHideKeys);
